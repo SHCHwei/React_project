31407e939c2d41454470a8ddac8a40f1ccc09410
Awei <wiwi328122@gmail.com>
2021-10-26 14:10:26 +0800

    更換年分

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component{
                     </Sider>
                 </Layout>
 
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2021 Created by Ant UED</Footer>
             </Layout>
         );
     }
