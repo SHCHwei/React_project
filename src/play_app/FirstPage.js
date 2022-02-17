407cfa84456167585798a26a40f550563e4fd039
Awei <wiwi328122@gmail.com>
2022-02-17 18:04:13 +0800

    輸入畫面+嘗試讀取json

--- src/play_app/FirstPage.js
+++ src/play_app/FirstPage.js
@@ -1,79 +1,84 @@
-import React from 'react';
-import {Layout, List, Button } from "antd";
+import React  from 'react';
+import {Layout, List, Button, Modal, Form, Input} from "antd";
+import jsonData from "src/file/team.json";
 
 const FirstPage=()=>{
-
     const { Header, Content,  Footer } = Layout;
 
+    console.log(jsonData);
 
-    const data = [
-        {
-            title: '廖育新',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
-        },
-        {
-            title: '許翊軒',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
-        },
-        {
-            title: 'JM',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
-        },
-        {
-            title: '富爸爸',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
-        },
-        {
-            title: '偉哥',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
-        },
-        {
-            title: '外籍洋將',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
-        },
-        {
-            title: '老K',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
-        },
-        {
-            title: '妹頭',
-            team: '彥彰,愷全,老大,正隆,昌育',
-            how_many: 6
+    const updateView = (state, action) => {
+        switch(action.type) {
+            case 'showModal':
+                return {...state, modalShow: true};
+            case 'offModal':
+                return {...state, modalShow: false};
         }
-    ];
+    }
+
+    const [state, dispatch] = React.useReducer(updateView, {
+        modalShow: true,
+        modalData: ''
+    });
+
 
     return(
-        <Layout className="layout">
-            <Header>
-                <div style={{textAlign: "center"}}>
-                    <Button type="primary">{"自己報一隊"}</Button>
-                </div>
-            </Header>
-            <Content style={{ padding: '0 30%' , background: '#d3d0d0'}}>
-                <div className="site-layout-content" style={{background: '#ffffff'}}>
-                    <List
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={ item => (
-                            <List.Item>
-                                <List.Item.Meta
-                                    title={item.title}
-                                    description={"隊伍人數 :"+item.how_many+ " , 隊員 :"+item.team}
-                                />
-                            </List.Item>
-                        )}
-                    />
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
+        <>
+            <Layout className="layout">
+                <Header>
+                    <div style={{textAlign: "center"}}>
+                        <Button type="primary" onClick={ ()=>dispatch({type: 'showModal'}) }>{"自己報一隊"}</Button>
+                    </div>
+                </Header>
+                <Content style={{ padding: '0 30%' , background: '#d3d0d0'}}>
+                    <div className="site-layout-content" style={{background: '#ffffff'}}>
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={jsonData}
+                            renderItem={ item => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        title={ <h3>{item.title}</h3> }
+                                        description={"隊伍人數 :"+item.how_many}
+                                    />
+                                    {"隊員 :"+item.team}
+                                </List.Item>
+                            )}
+                        />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2022 Created by Awei</Footer>
+            </Layout>
+
+            <Modal
+                title="報隊單"
+                visible={state.modalShow}
+                onOk={() => dispatch({type: 'offModal'}) }
+                onCancel={() => dispatch({type: 'offModal'}) }
+            >
+
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    autoComplete="off"
+                >
+                    <Form.Item label="報隊留名" name="who" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item label="人數" name="how_many" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item label="隊員" name="player">
+                        <Input />
+                    </Form.Item>
+                </Form>
+
+
+            </Modal>
+        </>
     )
 }
 
