971e4dc81231c4b7ac62aeb7ab2aa67379546c9a
Awei <wiwi328122@gmail.com>
2022-07-05 21:48:01 +0800

    完成取得隊伍串接

--- src/play_app/FirstPage.js
+++ src/play_app/FirstPage.js
@@ -1,6 +1,5 @@
 import React  from 'react';
 import {Layout, List, Button, Modal, Form, Input} from "antd";
-import {TeamList} from "./api.js";
 
 const updateView = (state, action) => {
     switch(action.type) {
@@ -17,7 +16,7 @@ const updateView = (state, action) => {
 const FirstPage=()=>{
     const { Header, Content,  Footer } = Layout;
     const [state, dispatch] = React.useReducer(updateView, {
-        modalShow: true,
+        modalShow: false,
         modalData: '',
         data: null
     });
@@ -25,9 +24,10 @@ const FirstPage=()=>{
 
     const getData = async () => {
 
-        const list = await TeamList();
-        console.log(list);
-        dispatch({type: "setData", data: list});
+        const response = await fetch('http://localhost:8099/');
+        const data = await response.json();
+
+        dispatch({type: "setData", data: data.Teams});
     }
 
 
@@ -35,67 +35,69 @@ const FirstPage=()=>{
 
         getData();
 
-    },[state.data]);
+    },[]);
 
 
-    return(state.data == null)?
-        <p>報撲類</p>:
-        <>
-            <Layout className="layout">
-                <Header>
-                    <div style={{textAlign: "center"}}>
-                        <Button type="primary" onClick={ ()=>dispatch({type: 'showModal'}) }>{"自己報一隊"}</Button>
-                    </div>
-                </Header>
-                <Content style={{ padding: '0 30%' , background: '#d3d0d0'}}>
-                    <div className="site-layout-content" style={{background: '#ffffff'}}>
-                        <List
-                            itemLayout="horizontal"
-                            dataSource={""}
-                            renderItem={ item => (
-                                <List.Item>
-                                    <List.Item.Meta
-                                        title={ <h3>{item.title}</h3> }
-                                        description={"隊伍人數 :"+item.how_many}
-                                    />
-                                    {"隊員 :"+item.team}
-                                </List.Item>
-                            )}
-                        />
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2022 Created by Awei</Footer>
-            </Layout>
+    return( <>
+        <Layout className="layout">
+            <Header>
+                <div style={{textAlign: "center"}}>
+                    <Button type="primary" onClick={ ()=>dispatch({type: 'showModal'}) }>{"自己報一隊"}</Button>
+                </div>
+            </Header>
+            <Content style={{ padding: '0 30%' , background: '#d3d0d0'}}>
+                <div className="site-layout-content" style={{background: '#ffffff'}}>
+                    {(state.data != null) ?
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={state.data}
+                        renderItem={ item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    title={item.title}
+                                    description={"隊伍人數 :"+item.how_many}
+                                />
+                                {"隊員 :"+item.team}
+                            </List.Item>
+                        )}
+                    />
+                    : <></>
+                    }
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2022 Created by Awei</Footer>
+        </Layout>
 
-            <Modal
-                title="報隊單"
-                visible={state.modalShow}
-                onOk={() => dispatch({type: 'offModal'}) }
-                onCancel={() => dispatch({type: 'offModal'}) }
+
+        <Modal
+            title="報隊單"
+            visible={state.modalShow}
+            onOk={() => dispatch({type: 'offModal'}) }
+            onCancel={() => dispatch({type: 'offModal'}) }
+        >
+
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                autoComplete="off"
             >
+                <Form.Item label="報隊留名" name="who" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
 
-                <Form
-                    name="basic"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
-                    autoComplete="off"
-                >
-                    <Form.Item label="報隊留名" name="who" rules={[{ required: true }]}>
-                        <Input />
-                    </Form.Item>
+                <Form.Item label="人數" name="how_many" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
 
-                    <Form.Item label="人數" name="how_many" rules={[{ required: true }]}>
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item label="隊員" name="player">
-                        <Input />
-                    </Form.Item>
-                </Form>
+                <Form.Item label="隊員" name="player">
+                    <Input />
+                </Form.Item>
+            </Form>
 
 
-            </Modal>
-        </>;
+        </Modal>
+    </>);
 }
 
 export default FirstPage;
